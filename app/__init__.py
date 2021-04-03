d80c7032d7b111cb7b98fefa2457825872c6eb26
Hank <hank.likes.matcha@gmail.com>
2021-04-04 09:55:33 +1200

    add post & get

--- app/__init__.py
+++ app/__init__.py
@@ -1,42 +1,50 @@
 from typing import List
 
 import uvicorn
-from fastapi import FastAPI
-
-from app.models import Order
+from fastapi import FastAPI, Depends
+from app import models as m
 
 from app.database import engine, Session
-from app import models
-from app.schemas import OrderType
+from app import models, schemas
 
 models.Base.metadata.create_all(engine)
 app = FastAPI()
-session = Session()
+
+
+def db_connection():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
 
 
 @app.get("/")
 def index():
-    return dict(data="hello world")
+    return dict(data="landing page")
 
 
 @app.get("/order/{id}")
-def view_order(id: str):
-    return dict(data=id)
+def view_order(id: str, db: Session = Depends(db_connection)):
+    order: m.Order = db.query(m.Order).filter_by(id=id).first()
+    return dict(data=order)
 
 
 @app.get("/orders")
-def view_orders():
-    orders: List[Order] = session.query(Order).all()
+def view_orders(db: Session = Depends(db_connection)):
+    orders: List[m.Order] = db.query(m.Order).all()
 
     return dict(data=orders)
 
 
-#
-
-
 @app.post("/order")
-def create_order(request: OrderType):
-    return dict(data=Order)
+def create_order(request: schemas.OrderType, db: Session = Depends(db_connection)):
+    new_order: m.Order = m.Order(customer=request.customer, code=request.code)
+    db.add(new_order)
+    db.commit()
+    db.refresh(new_order)
+
+    return new_order
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -4,11 +4,13 @@ from sqlalchemy import Column, DateTime, String
 
 from app.database import Base
 from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class Order(Base):
     __tablename__ = "order"
 
-    id = Column(String, primary_key=True, default=uuid4())
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
     created = Column(DateTime, default=datetime.datetime.utcnow)
+    code = Column(String)
     customer = Column(String)
