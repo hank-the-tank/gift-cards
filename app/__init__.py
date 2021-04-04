6117b75127b00ab67b7fe49cc42ab8ea1ccba7d6
Hank <hank.likes.matcha@gmail.com>
2021-04-04 16:05:00 +1200

    CRUD Done

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+from uuid import UUID
+
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from typing import List
 
 import uvicorn
-from fastapi import FastAPI, Depends
 from app import models as m
 
 from app.database import engine, Session
@@ -24,9 +26,13 @@ def index():
     return dict(data="landing page")
 
 
-@app.get("/order/{id}")
-def view_order(id: str, db: Session = Depends(db_connection)):
+@app.get("/order/{id}", status_code=status.HTTP_200_OK)
+def view_order(id: UUID, response: Response, db: Session = Depends(db_connection)):
     order: m.Order = db.query(m.Order).filter_by(id=id).first()
+    if not order:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
+        )
     return dict(data=order)
 
 
@@ -37,14 +43,51 @@ def view_orders(db: Session = Depends(db_connection)):
     return dict(data=orders)
 
 
-@app.post("/order")
-def create_order(request: schemas.OrderType, db: Session = Depends(db_connection)):
-    new_order: m.Order = m.Order(customer=request.customer, code=request.code)
-    db.add(new_order)
-    db.commit()
-    db.refresh(new_order)
+@app.post("/order", status_code=status.HTTP_201_CREATED)
+def create_order(
+    request: schemas.OrderType, response: Response, db: Session = Depends(db_connection)
+):
+    if db.query(m.Order).filter_by(code=request.code).first():
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Order already existed"
+        )
+    else:
+        new_order: m.Order = m.Order(customer=request.customer, code=request.code)
+        db.add(new_order)
+        db.commit()
+        db.refresh(new_order)
 
-    return new_order
+        return new_order
+
+
+@app.delete("/order/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_order(id: UUID, db: Session = Depends(db_connection)):
+    order = db.query(m.Order).filter(m.Order.id == id)
+    if not order.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
+        )
+    else:
+        order.delete(synchronize_session=False)
+        db.commit()
+
+    return "Deleted"
+
+
+@app.put("/order/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_order(
+    id: UUID, request: schemas.OrderType, db: Session = Depends(db_connection)
+):
+    order = db.query(m.Order).filter(m.Order.id == id)
+    if not order.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
+        )
+    else:
+        order.update(dict(code=request.code, customer=request.customer))
+        db.commit()
+
+    return "Updated"
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -1,16 +1,17 @@
 import datetime
 
-from sqlalchemy import Column, DateTime, String
+import sqlalchemy as sa
 
+from fastapi_utils.guid_type import GUID, GUID_SERVER_DEFAULT_POSTGRESQL
 from app.database import Base
-from uuid import uuid4
-from sqlalchemy.dialects.postgresql import UUID
 
 
 class Order(Base):
     __tablename__ = "order"
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
-    created = Column(DateTime, default=datetime.datetime.utcnow)
-    code = Column(String)
-    customer = Column(String)
+    # create a ufnction for generating uuid
+    id = sa.Column(
+        GUID, primary_key=True, server_default=GUID_SERVER_DEFAULT_POSTGRESQL
+    )
+    created = sa.Column(sa.DateTime, default=datetime.datetime.utcnow)
+    code = sa.Column(sa.String)
+    customer = sa.Column(sa.String)
